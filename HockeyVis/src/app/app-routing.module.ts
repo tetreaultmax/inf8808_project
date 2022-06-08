de48cb725c5383742d0295702f4db1eaecbef722
Arnaudess <arnaud.dessain@outlook.com>
2022-06-08 11:42:59 -0400

    init

---
+++ HockeyVis/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BackToBackChartComponent } from './back-to-back-chart/back-to-back-chart.component';
+
+const routes: Routes = [
+  { path: 'backToBack', component: BackToBackChartComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
