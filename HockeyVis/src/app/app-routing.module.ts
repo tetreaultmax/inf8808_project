4bbfcdf98e3413ddd46adcba3e29fc87c9b303cc
Maxime Tétreault <maxime.tetreault@polymtl.ca>
2022-06-21 19:04:07 -0400

    csv points

--- HockeyVis/src/app/app-routing.module.ts
+++ HockeyVis/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BackToBackChartComponent } from './back-to-back-chart/back-to-back-chart.component';
+import { SlopeChartComponent } from './slope-chart/slope-chart.component';
 import { StackedBarChartComponent } from './stacked-bar-chart/stacked-bar-chart.component';
 import { UnivariateScatterPlotComponent } from './univariate-scatter-plot/univariate-scatter-plot.component';
 
 const routes: Routes = [
   { path: 'backToBack', component: BackToBackChartComponent },
   { path: 'stackedBar', component: StackedBarChartComponent },
-  { path: 'univariateScatter', component: UnivariateScatterPlotComponent }
+  { path: 'univariateScatter', component: UnivariateScatterPlotComponent },
+  { path: 'slopeChart', component: SlopeChartComponent }
+
 ];
 
 @NgModule({
--- HockeyVis/src/app/slope-chart/slope-chart.component.ts
+++ HockeyVis/src/app/slope-chart/slope-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-slope-chart',
@@ -8,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class SlopeChartComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit() {
+	const YEARS = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+	YEARS.forEach(year =>{
+		const path = "/assets/stats/stats_" + String(year)+".csv"
+		d3.csv(path).then(data =>{
+			console.log(data)
+		})
+	})
   }
 
 }
