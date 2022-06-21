8fc58b25dca5bef0c6f6bd3f43ed8c40bd9152f6
Maxime Tétreault <maxime.tetreault@polymtl.ca>
2022-06-21 17:20:13 -0400

    read csv stacked

--- HockeyVis/src/app/stacked-bar-chart.service.ts
+++ HockeyVis/src/app/stacked-bar-chart.service.ts
@@ -15,40 +15,34 @@ interface StackedChart{
   providedIn: 'root'
 })
 export class StackedBarChartService {
-
 	constructor(){
-		var data = [
-			{year: 2007, shots: 65294, misses: 26889, blocked: 29648, goals: 7034},
-			{year: 2008, shots: 67671, misses: 28492, blocked: 32237, goals: 7345},
-			{year: 2009, shots: 68390, misses: 29017, blocked: 33422, goals: 7245},
-			{year: 2010, shots: 68580, misses: 29108, blocked: 34902, goals: 7045},
-			{year: 2011, shots: 67223, misses: 28686, blocked: 34796, goals: 6954},
-			{year: 2012, shots: 38467, misses: 16536, blocked: 20597, goals: 4048},
-			{year: 2013, shots: 68009, misses: 29067, blocked: 34757, goals: 6986},
-			{year: 2014, shots: 67704, misses: 28776, blocked: 35598, goals: 6929},
-			{year: 2015, shots: 66979, misses: 28635, blocked: 34845, goals: 6803},
-			{year: 2016, shots: 68030, misses: 29123, blocked: 34994, goals: 6928},
-			{year: 2017, shots: 74176, misses: 31195, blocked: 37423, goals: 7691},
-			{year: 2018, shots: 72708, misses: 30392, blocked: 36261, goals: 7765},
-			{year: 2019, shots: 61843, misses: 25261, blocked: 29615, goals: 6648},
-			{year: 2020, shots: 47221, misses: 19350, blocked: 22554, goals: 5176},
-			{year: 2021, shots: 75232, misses: 30424, blocked: 35369, goals: 8374}
-		  ];
-		var colors = ['#7FFFD4', '#D2691E', '#8B0000', '#808080'];
-		var categories = ["shots", "misses", "blocked", "goals"]
-		var margin = 50;
-		var marginTop = 50
-		var marginSide = 100
-		var width = 0.9 * window.innerWidth;
-		var height = 0.8* window.innerHeight;
-		var widthChart = 0.7 * width
-		var heightChart = 0.9 * height
-		var placeLegend = widthChart + 10
-		var spaceLegend = 50
-		var radius = 10
-		var keys = ["Buts", "Tirs bloqués", "Tirs ratés", "Tirs tentés"]
-		var stackedData = d3.stack().keys(categories)(data as any)
-		const buildBarChart = () => {
+		d3.csv("/assets/shot_data.csv").then((data) =>{
+			const new_data: { year: number; shots: number; misses: number; blocked: number; goals: number; }[] = []
+			data.forEach(d => {
+				const year = Number(d['year'])
+				const shots = Number(d['shot'])
+				const misses = Number(d['miss'])
+				const blocked = Number(d['block'])
+				const goals = Number(d['goal'])
+				new_data.push({year, shots, misses, blocked, goals})
+			})
+			
+			var colors = ['#7FFFD4', '#D2691E', '#8B0000', '#808080'];
+			var categories = ["shots", "misses", "blocked", "goals"]
+			var margin = 50;
+			var marginTop = 50
+			var marginSide = 100
+			var width = 0.9 * window.innerWidth;
+			var height = 0.8* window.innerHeight;
+			var widthChart = 0.7 * width
+			var heightChart = 0.9 * height
+			var placeLegend = widthChart + 10
+			var spaceLegend = 50
+			var radius = 10
+			var keys = ["Buts", "Tirs bloqués", "Tirs ratés", "Tirs tentés"]
+			var stackedData = d3.stack().keys(categories)(new_data)
+
+			const buildBarChart = () => {
 			var svg = d3.select("#stackedBar")
 				.append("svg")
 				.attr('class', 'stacked-chart')
@@ -58,7 +52,7 @@ export class StackedBarChartService {
 				.attr("width", widthChart)
 				.attr("height",heightChart)
 				.attr("transform", 'translate(' + 1.2*marginSide + ',' + marginTop + ')')
-				
+
 			svg.selectAll("mydots")
 				.data(keys)
 				.enter()
@@ -79,13 +73,13 @@ export class StackedBarChartService {
 				.style('font-family', 'Helvetica')
 				.style('font-size', 20)
 				.attr("transform", 'translate(' + (placeLegend + 10) + ',0)')
-				
-			var domain = data.map(function(d) { return String(d.year); })
+
+			var domain = data.map(function(d) { return String(d['year']); })
 			var xScale = d3.scaleBand().padding(1).domain(domain).range([0, widthChart])
 
 			let max = 0
 			data.forEach(d => {
-				const total = d.goals + d.misses + d.shots + d.blocked
+				const total = Number(d['goal']) + Number(d['miss']) + Number(d['shot']) + Number(d['block'])
 				if (total > max) {
 					max = total
 				}
@@ -96,15 +90,15 @@ export class StackedBarChartService {
 				.range([heightChart - margin, 0]);
 
 			var yAxis = d3.axisLeft(yScale).tickSize(-widthChart)
-	
+
 			var xAxis = d3.axisBottom(xScale)
-	
+
 			svg.append("g")
 				.attr("class", "y axis")
 				.style('font-family', 'Helvetica')
 				.style('font-size', 20)
 				.call(yAxis);
-	
+
 			svg.append("g")
 				.attr("class", "x axis")
 				.attr("transform", "translate(0," + (heightChart - margin) + ")")
@@ -134,8 +128,8 @@ export class StackedBarChartService {
 				.enter().append("g")
 				.attr("class", "bars")
 				.style("fill", function(d, i) { return colors[i]; })
-				
-			
+
+
 
 			groups.selectAll("rect")
 				.data(function(d) { return d; })
@@ -150,9 +144,9 @@ export class StackedBarChartService {
 					d3.select('#tooltip').text(`Fréquence de tirs: ${d[1] - d[0]}`).style('opacity', 1).style('font-family', 'Helvetica')
 					.style('font-size', 20)
 				  })
-				.on('mouseout', () => d3.select('#tooltip').style('opacity', 0));		
-		}
-		setTimeout( buildBarChart, 500)
-		
+				.on('mouseout', () => d3.select('#tooltip').style('opacity', 0));
+			}
+			setTimeout( buildBarChart, 500)
+		})
 	}
 }
